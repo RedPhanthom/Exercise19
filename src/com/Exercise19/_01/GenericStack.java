e91bd220f05e75c23eaf8ce2a0e8c9b6b626ca87
RedPhanthom <RedPhanthom@outlook.com>
2017-01-16 16:27:15 -0500

    Project Structure

---
+++ src/com/Exercise19/_01/GenericStack.java
@@ -0,0 +1,18 @@
+/*--------------------------------------------------------------------
+Name: Jaime Palencia
+Student ID: U94665425
+COP 2805C - Java Programming 2
+Spring /2017
+Assignment # 2
+Plagiarism Statement
+I certify that this assignment is my own work and that I
+have not copied in part or whole or otherwise plagiarized 
+the work of other students and/or persons.
+--------------------------------------------------------------------*/
+package com.Exercise19._01;
+
+public class GenericStack {
+	public static void main(String[] args) {
+
+	}
+}
